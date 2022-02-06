c5bd66e7390933e41ef88ab91dd0cc4e3f3ed93c
Hamidreza Etebarian <onehamidreza@gmail.com>
2022-02-07 01:23:46 +0330

    Improved Performance and removed lazy.

--- src/components/CurvedBottomBar/components/navigator/index.tsx
+++ src/components/CurvedBottomBar/components/navigator/index.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useImperativeHandle, useMemo, useRef, useState } from 'react';
-import { Dimensions, View } from 'react-native';
+import React, {Ref, useCallback, useEffect, useImperativeHandle, useMemo, useRef, useState} from 'react';
+import {Dimensions, View} from 'react-native';
 import PagerView from 'react-native-pager-view';
-import Svg, { Path } from 'react-native-svg';
-import { useDeviceOrientation } from '../../../useDeviceOrientation';
-import { getPath, getPathUp } from './path';
-import { styles } from './styles';
-import { NavigatorBottomBar } from './type';
+import Svg, {Path} from 'react-native-svg';
+import {useDeviceOrientation} from '../../../useDeviceOrientation';
+import {getPath, getPathUp} from './path';
+import {styles} from './styles';
+import {NavigatorBottomBar, NavigatorBottomMenu, MenuProps} from './type';
 
 const defaultProps = {
   bgColor: 'gray',
@@ -28,46 +28,32 @@ const BottomBarComponent: NavigatorBottomBar = React.forwardRef((props, ref) => 
     borderTopLeftRight,
     strokeWidth,
     swipeEnabled = false,
-    lazy = false,
   } = props;
+  // console.log('BottomNavigation Render')
 
-  const [selectedTab, setselectedTab] = useState<string>(initialRouteName);
-  const [itemLeft, setItemLeft] = useState([]);
-  const [itemRight, setItemRight] = useState([]);
-  const [maxWidth, setMaxWidth] = useState<any>(width);
+  const selectedTabVariable = useState<string>(initialRouteName);
   const children = props?.children as any[];
-  const orientation = useDeviceOrientation();
   const refPageView: any = useRef(null);
   const containerRef: any = useRef(null);
-  const [lazyList] = useState<Boolean[]>([...Array(children.length)].map((item, index) => {
-    return false;
-  }));
+  const menuRef: any = useRef(null);
 
   useImperativeHandle(ref, () => {
-    return { navigate: navigate, getRouteName: selectedTab, setStyle, };
+    return {navigate: navigate, getRouteName: selectedTabVariable[0], setStyle,};
   });
 
-  const navigate = (routeName: string) => {
-    setRouteName(routeName);
+  const navigate = (name: string) => {
+    updateSelectedTab(name)
+    const index = children.findIndex(e => e.props?.name == name);
+    if (index >= 0) {
+      refPageView.current.setPageWithoutAnimation(index);
+    }
   };
 
   const setStyle = (style: any) => {
-    containerRef?.current?.setNativeProps({style:[styles.container, style]})
+    containerRef?.current?.setNativeProps({style: [styles.container, style]})
   };
 
-
-  useEffect(() => {
-    const { width: w, height: h } = Dimensions.get('window');
-    if (!width) {
-      setMaxWidth(w);
-    }
-  }, [orientation]);
-
-  const _renderButtonCenter = () => {
-    return renderCircle({ selectedTab, navigate });
-  };
-
-  const selectedTabIndex = useMemo(() => {
+  const selectedInitialTabIndex = useMemo(() => {
     const index = children.findIndex(e => e.props?.name == initialRouteName);
     if (index >= 0) {
       return index;
@@ -75,111 +61,164 @@ const BottomBarComponent: NavigatorBottomBar = React.forwardRef((props, ref) => 
     return 0;
   }, [initialRouteName]);
 
-  useEffect(() => {
-    const arrLeft: any = children.filter((item) => item?.props?.position === 'left');
-    const arrRight: any = children.filter((item) => item?.props?.position === 'right');
-
-    setItemLeft(arrLeft);
-    setItemRight(arrRight);
-    setRouteName(initialRouteName);
-  }, []);
-
-  const setRouteName = (name: string) => {
-    setselectedTab(name);
-    const index = children.findIndex(e => e.props?.name == name);
-    if (index >= 0) {
-      refPageView.current.setPageWithoutAnimation(index);
-    }
-  };
-
   const onPageSelected = (index: number) => {
-    setselectedTab(children[index].props?.name);
+    updateSelectedTab(children[index].props?.name)
   };
 
-  const tabSelected = useMemo(() => {
-    const selectIndex = children.findIndex(e => e.props?.name == selectedTab);
-    lazyList[selectIndex] = true;
-    return lazyList
-  }, [selectedTab]);
+  const updateSelectedTab = useCallback((name) => {
+    selectedTabVariable[0] = name
+    menuRef?.current?.updateSelectedTab(name)
+  }, [menuRef, menuRef?.current])
 
   const _renderTab = (item: any, index: number) => {
-    if (lazy) {
-      return (<View
-        key={index.toString()}
-        style={{ flex: 1 }}>
-        {item.props.renderHeader && item.props.renderHeader({ navigate: (routeName: string) => { setRouteName(routeName) } })}
-        {tabSelected[index] && item.props.component({ navigate: (routeName: string) => { setRouteName(routeName) } })}
-      </View>)
-    } else {
-      return (<View
-        key={index.toString()}
-        style={{ flex: 1 }}>
-        {item.props.renderHeader && item.props.renderHeader({ navigate: (routeName: string) => { setRouteName(routeName) } })}
-        {item.props.component({ navigate: (routeName: string) => { setRouteName(routeName) } })}
-      </View>)
-    }
+    return (<View
+      key={index.toString()}
+      style={{flex: 1}}>
+      {item.props.renderHeader && item.props.renderHeader({
+        navigate: (routeName: string) => {
+          navigate(routeName)
+        }
+      })}
+      {item.props.component({
+        navigate: (routeName: string) => {
+          navigate(routeName)
+        }
+      })}
+    </View>)
   };
 
-  const d = type === 'down' ? getPath(maxWidth, height, circleWidth >= 50 ? circleWidth : 50, borderTopLeftRight) : getPathUp(maxWidth, height + 30, circleWidth >= 50 ? circleWidth : 50, borderTopLeftRight);
-  if (d) {
-    return (
-      <View style={{ flex: 1 }}>
-        <PagerView
-          ref={refPageView}
-          style={{ flex: 1 }}
-          initialPage={selectedTabIndex}
-          scrollEnabled={lazy ? false : swipeEnabled}
-          onPageSelected={e => onPageSelected(e.nativeEvent.position)}
-        >
-          {children.map(_renderTab)}
-        </PagerView>
+  useEffect(() => {
+    navigate(initialRouteName);
+  }, []);
 
-        <View ref={containerRef} style={[styles.container, style]}>
-          <Svg width={maxWidth} height={height + (type === 'down' ? 0 : 30)}>
-            <Path fill={bgColor} stroke="#DDDDDD" strokeWidth={strokeWidth} {...{ d }} />
-          </Svg>
-          <View style={[styles.main, { width: maxWidth }, type === 'up' && { top: 30 }]}>
-            <View style={[styles.rowLeft, { height: height }]}>
-              {itemLeft.map((item: any, index) => {
-                const routeName: string = item?.props?.name;
-
-                return (
-                  <View style={{ flex: 1 }} key={index}>
-                    {tabBar({
-                      routeName,
-                      selectedTab: selectedTab,
-                      navigate: (selectedTab: string) => {
-                        setRouteName(selectedTab);
-                      },
-                    })}
-                  </View>
-                );
-              })}
-            </View>
-            {_renderButtonCenter()}
-            <View style={[styles.rowRight, { height: height }]}>
-              {itemRight.map((item: any, index) => {
-                const routeName = item?.props?.name;
-                return (
-                  <View style={{ flex: 1 }} key={index}>
-                    {tabBar({
-                      routeName,
-                      selectedTab: selectedTab,
-                      navigate: (selectedTab: string) => {
-                        setRouteName(selectedTab);
-                      },
-                    })}
-                  </View>
-                );
-              })}
-            </View>
-          </View>
-        </View>
-      </View>
-    );
-  }
-  return null;
+  return (<View style={{flex: 1}}>
+      <PagerView
+        ref={refPageView}
+        style={{flex: 1}}
+        initialPage={selectedInitialTabIndex}
+        scrollEnabled={swipeEnabled}
+        onPageSelected={e => onPageSelected(e.nativeEvent.position)}
+      >
+        {children.map(_renderTab)}
+      </PagerView>
+      <BottomMenu
+        ref={menuRef}
+        containerRef={containerRef}
+        navigate={navigate}
+        style={[styles.container, style]}
+        width={width}
+        height={height}
+        arrLeft={children.filter((item) => item?.props?.position === 'left')}
+        arrRight={children.filter((item) => item?.props?.position === 'right')}
+        type={type}
+        bgColor={bgColor}
+        strokeWidth={strokeWidth}
+        tabBar={tabBar}
+        circleWidth={circleWidth}
+        renderCircle={renderCircle}
+        borderTopLeftRight={borderTopLeftRight}
+      />
+    </View>
+  );
 });
+
+const BottomMenu = React.forwardRef((props: MenuProps, ref: Ref<NavigatorBottomMenu>) => {
+  const {
+    containerRef,
+    navigate,
+    arrLeft,
+    arrRight,
+    initialRouteName,
+    width,
+    height,
+    type,
+    bgColor,
+    strokeWidth,
+    tabBar,
+    circleWidth,
+    renderCircle,
+    borderTopLeftRight,
+    style
+  } = props
+  const [selectedTab, setSelectedTab] = useState()
+  const orientation = useDeviceOrientation();
+  const svgRef = useRef(null);
+  const pathRef = useRef(null);
+  const iconContainerRef = useRef(null);
+
+  const [itemLeft, setItemLeft] = useState([]);
+  const [itemRight, setItemRight] = useState([]);
+
+  useImperativeHandle(ref, () => {
+    return {updateSelectedTab: updateSelectedTab};
+  });
+
+  useEffect(() => {
+    const {width: w, height: h} = Dimensions.get('window');
+    if (!width) {
+      const d = type === 'down' ? getPath(w, height, circleWidth >= 50 ? circleWidth : 50, borderTopLeftRight) : getPathUp(w, height + 30, circleWidth >= 50 ? circleWidth : 50, borderTopLeftRight);
+
+      svgRef?.current?.setNativeProps({width: w})
+      pathRef?.current?.setNativeProps({d})
+      iconContainerRef?.current?.setNativeProps({style: [styles.main, {width: w}, type === 'up' && {top: 30}]})
+    }
+  }, [type, orientation, svgRef, svgRef?.current, pathRef, pathRef?.current, iconContainerRef, iconContainerRef?.current]);
+
+  const _renderButtonCenter = useCallback(() => {
+    return renderCircle({selectedTab, navigate});
+  }, [selectedTab, navigate]);
+
+  function updateSelectedTab(name) {
+    setSelectedTab(name)
+  }
+
+  useEffect(() => {
+    setItemLeft(arrLeft);
+    setItemRight(arrRight);
+  }, []);
+
+  return (<View ref={containerRef} style={[styles.container, style]}>
+    <Svg ref={svgRef} height={height + (type === 'down' ? 0 : 30)}>
+      <Path ref={pathRef} fill={bgColor} stroke="#DDDDDD" strokeWidth={strokeWidth}/>
+    </Svg>
+    <View ref={iconContainerRef}>
+      <View style={[styles.rowLeft, {height: height}]}>
+        {itemLeft.map((item: any, index) => {
+          const routeName: string = item?.props?.name;
+
+          return (
+            <View style={{flex: 1}} key={index}>
+              {tabBar({
+                routeName,
+                selectedTab: selectedTab,
+                navigate: (selectedTab: string) => {
+                  navigate(selectedTab);
+                },
+              })}
+            </View>
+          );
+        })}
+      </View>
+      {_renderButtonCenter()}
+      <View style={[styles.rowRight, {height: height}]}>
+        {itemRight.map((item: any, index) => {
+          const routeName = item?.props?.name;
+          return (
+            <View style={{flex: 1}} key={index}>
+              {tabBar({
+                routeName,
+                selectedTab: selectedTab,
+                navigate: (selectedTab: string) => {
+                  navigate(selectedTab);
+                },
+              })}
+            </View>
+          );
+        })}
+      </View>
+    </View>
+  </View>)
+})
 
 BottomBarComponent.defaultProps = defaultProps;
 
--- src/components/CurvedBottomBar/components/navigator/type.ts
+++ src/components/CurvedBottomBar/components/navigator/type.ts
@@ -2,33 +2,68 @@ import React from 'react';
 import { StyleProp, ViewStyle } from 'react-native';
 
 interface Props {
-    type?: 'down' | 'up' | string;
-    style?: StyleProp<ViewStyle>;
-    width?: number;
-    height?: number;
-    borderTopLeftRight?: boolean;
-    circleWidth?: number;
-    bgColor?: string;
-    initialRouteName: string;
-    strokeWidth?: number;
-    swipeEnabled?: boolean;
-    lazy?: boolean;
-    renderCircle: ({
-      selectedTab,
-      navigate,
-    }: {
-      selectedTab: string;
-      navigate: (selectedTab: string) => void;
-    }) => JSX.Element;
-    tabBar: ({
-      routeName,
-      selectedTab,
-      navigate,
-    }: {
-      routeName: string;
-      selectedTab: string;
-      navigate: (selectedTab: string) => void;
-    }) => JSX.Element;
+  type?: 'down' | 'up' | string;
+  style?: StyleProp<ViewStyle>;
+  width?: number;
+  height?: number;
+  borderTopLeftRight?: boolean;
+  circleWidth?: number;
+  bgColor?: string;
+  initialRouteName: string;
+  strokeWidth?: number;
+  swipeEnabled?: boolean;
+  lazy?: boolean;
+  renderCircle: ({
+                   selectedTab,
+                   navigate,
+                 }: {
+    selectedTab: string;
+    navigate: (selectedTab: string) => void;
+  }) => JSX.Element;
+  tabBar: ({
+             routeName,
+             selectedTab,
+             navigate,
+           }: {
+    routeName: string;
+    selectedTab: string;
+    navigate: (selectedTab: string) => void;
+  }) => JSX.Element;
 }
 
 export type NavigatorBottomBar = React.FC<Props>
+
+export interface MenuProps {
+  containerRef: any;
+  navigate: (name:string) => void;
+  arrLeft : [];
+  arrRight: [];
+  initialRouteName: string;
+  width:number;
+  height:number;
+  type?: 'down' | 'up' | string;
+  bgColor?: string;
+  strokeWidth?: number;
+  tabBar: ({
+             routeName,
+             selectedTab,
+             navigate,
+           }: {
+    routeName: string;
+    selectedTab: string;
+    navigate: (selectedTab: string) => void;
+  }) => JSX.Element;
+  circleWidth?:number;
+  renderCircle: ({
+                   selectedTab,
+                   navigate,
+                 }: {
+    selectedTab: string;
+    navigate: (selectedTab: string) => void;
+  }) => JSX.Element;
+  borderTopLeftRight?: boolean;
+  style?: StyleProp<ViewStyle>;
+  updateSelectedTab : (name:string) => void
+}
+
+export type NavigatorBottomMenu = React.FC<MenuProps>
